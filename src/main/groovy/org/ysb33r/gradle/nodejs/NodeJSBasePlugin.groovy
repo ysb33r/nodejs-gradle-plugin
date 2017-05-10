9e9f5a5c7d6fd196d80dc585c4991a5619af7ccc
Schalk W. Cronjé <yw-github@af.org.za>
2017-05-11 01:45:37 +0200

    Add a base plugin

---
+++ src/main/groovy/org/ysb33r/gradle/nodejs/NodeJSBasePlugin.groovy
@@ -0,0 +1,17 @@
+package org.ysb33r.gradle.nodejs
+
+import groovy.transform.CompileStatic
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/** Provide the basic capabilites for dealing with Node.js & NPM tasks. Allow for downlaoding & caching of Node.js distributions
+ * on a variery of the most common development platforms.
+ *
+ * @since 0.1
+ */
+@CompileStatic
+class NodeJSBasePlugin implements Plugin<Project> {
+    void apply(Project project) {
+
+    }
+}
